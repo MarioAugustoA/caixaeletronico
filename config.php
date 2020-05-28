61429f9fb340d257abca4e016df6ff5173602ceb
Mario Augusto <mario_abreucardoso@hotmail.com>
2020-05-28 00:08:03 -0300

    ultimo commit

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php  
 
 try{
-$pdo = new PDO("mysql:dbname=projeto_caixaeletronico; host=localhost","root","");
+$pdo = new PDO("mysql:dbname=projeto_caixaeletronico; host=localhost","","");
 
 }catch(PDOExcepetion $e){
 	echo "Erro".$e->getMessage();
